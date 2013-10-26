e004bb17d36b07b2f4e3315e597f6449a2caf944
Tao Yu <yutaoitper@gmail.com>
2013-10-26 01:42:47 -0400

    horizontal_scaling

---
+++ horizontal_scaling/generate_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import csv
+
+def main():
+    
+
+    value = 0
+    for num in range(1, 52):
+        f = open("week"+str(num)+".csv","a")
+        i = 0
+        with open("week"+str(num-1)+".csv", 'rw') as csvfile:
+            for row in csvfile:
+                try:
+                   value = float(str(row.split(",")[1].strip()))*((1+0.12/52))
+                   line = str(i) + "," + str(value)
+                   print str(line)+"\n"
+                   f.write(str(line)+"\n")
+                except ValueError,e:
+                   print e
+                i = i + 1
+                
+       
+if __name__ == "__main__":
+    main()
+    
